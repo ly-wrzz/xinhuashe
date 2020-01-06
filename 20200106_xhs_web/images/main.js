82ff097a8fd9804e23dfd29ec69bb49a1d9dcd1c
Happiness <oncwnuOR4cihZolndZA2vhRLYvN8@git.weixin.qq.com>
2020-01-06 23:30:00 +0800

    [+]修改代码

--- 20200106_xhs_web/images/main.js
+++ 20200106_xhs_web/images/main.js
@@ -1,5 +1,12 @@
-"use strict";
+'use strict';
 
 $(function () {
     console.log(123);
+    // 现场直击导航切换js
+    $('.picture-title li').on('click', function () {
+        var index = $(this).index() + 1;
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.picture-list').hide();
+        $('.picture-list' + index).show();
+    });
 });
--- 20200106_xhs_web/res/scripts/main.js
+++ 20200106_xhs_web/res/scripts/main.js
@@ -1,3 +1,10 @@
 $(function () {
     console.log(123);
+    // 现场直击导航切换js
+    $('.picture-title li').on('click', function(){
+        const index = $(this).index() + 1;
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.picture-list').hide();
+        $(`.picture-list${index}`).show();
+    });
 });
