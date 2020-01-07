66cd9cc0135d867f1d8003a93600df6cba62889a
leexhuan <17607165156@163.com>
2020-01-07 18:38:56 +0800

    [+]增加代码

--- 20200106_xhs_web/res/scripts/main.js
+++ 20200106_xhs_web/res/scripts/main.js
@@ -2,22 +2,7 @@ $(function () {
     const xinHuaShe = {
         init: function() {
             this.OnSiteDirectStrike();
-            var count = $('.slide_box .bd ul li').length;
-            var pagination = [];
-            for(var i=0;i<count;i++){
-                pagination.push('<li>'+(i+1)+'</li>')
-            }
-            pagination = '<div class="hd"><ul>'+pagination.join('')+'</ul></div>';
-            $('.slideTxtBox').slide({
-                mainCell:'.bd ul',
-                effect:'left',
-                trigger:'mouseover',
-                autoPlay:true,
-                easing:'swing',
-                delayTime:800,
-                mouseOverStop:true,
-                pnLoop:true
-            })
+            this.GoodVoiceAction();
         },
         // 现场直击
         OnSiteDirectStrike: function() {
@@ -28,6 +13,23 @@ $(function () {
                 $('.picture-list').hide();
                 $(`.picture-list${index}`).show();
             });
+        },
+        // 好声音轮播
+        GoodVoiceAction: function() {
+            const count = $('.slide_box .bd ul li').length;
+            let pagination = [];
+            Array.from({length: count}, (item, idx) => {pagination.push(`<li>${idx+1}</li>`)})
+            pagination = `<div class="hd"><ul>${pagination.join('')}</ul></div>`;
+            $('.slideTxtBox').slide({
+                mainCell:'.bd ul',
+                effect:'left',
+                trigger:'mouseover',
+                autoPlay:true,
+                easing:'swing',
+                delayTime:800,
+                mouseOverStop:true,
+                pnLoop:true
+            })
         }
     };
     xinHuaShe.init();
