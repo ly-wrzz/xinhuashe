bde05226e72cb0623c767286ed407c946dd89689
ly <1503221964@qq.com>
2020-01-07 15:32:02 +0800

    可以轮

--- 20200106_xhs_web/res/scripts/main.js
+++ 20200106_xhs_web/res/scripts/main.js
@@ -2,6 +2,22 @@ $(function () {
     const xinHuaShe = {
         init: function() {
             this.OnSiteDirectStrike();
+            var count = $('.slide_box .bd ul li').length;
+            var pagination = [];
+            for(var i=0;i<count;i++){
+                pagination.push('<li>'+(i+1)+'</li>')
+            }
+            pagination = '<div class="hd"><ul>'+pagination.join('')+'</ul></div>';
+            $('.slideTxtBox').slide({
+                mainCell:'.bd ul',
+                effect:'left',
+                trigger:'mouseover',
+                autoPlay:true,
+                easing:'swing',
+                delayTime:800,
+                mouseOverStop:true,
+                pnLoop:true
+            })
         },
         // 现场直击
         OnSiteDirectStrike: function() {
